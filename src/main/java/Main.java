570b65fe6af091544a5a63314a6589ffb12e999f
sabby155 <nebula155@gmail.com>
2022-06-22 15:34:52 -0400

    loop lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,14 @@ public class Main {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear;
+        int currentYear = startingYear + 1;
         while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
             // conditional logic based on the current year
+            if (currentYear - startingYear == 1) {
+                System.out.println((currentYear - startingYear) + " year has passed");
+            } else {
+                System.out.println((currentYear - startingYear) + " year(s) have passed");
+            }
             currentYear++;
         }
     }
