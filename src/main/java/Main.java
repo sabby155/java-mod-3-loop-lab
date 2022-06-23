624e082c7a5b86ed091f94c99a40e9b0dbe5342f
sabby155 <nebula155@gmail.com>
2022-06-23 12:03:45 -0400

    modify Loop Lab to include extension

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,35 @@
 public class Main {
     public static void main(String[] args) {
         // simulate the passing of time
+        // USING WHILE LOOP: 
+
+        // int startingYear = 2000;
+        // int targetYear = 2011;
+        // int currentYear = startingYear + 1;
+
+        // while (currentYear < targetYear) {
+        //     // conditional logic based on the current year
+        //     if (currentYear - startingYear == 1) {
+        //         System.out.println((currentYear - startingYear) + " year has passed");
+        //     } else {
+        //         System.out.println((currentYear - startingYear) + " year(s) have passed");
+        //     }
+        //     currentYear++;
+        // }
+
+        // simulate the passing of time
+        // USING FOR LOOP:
+
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear + 1;
-        while (currentYear < targetYear) {
+
+        for (int currentYear = startingYear + 1; currentYear < targetYear ; currentYear++) {
             // conditional logic based on the current year
             if (currentYear - startingYear == 1) {
                 System.out.println((currentYear - startingYear) + " year has passed");
             } else {
                 System.out.println((currentYear - startingYear) + " year(s) have passed");
             }
-            currentYear++;
         }
     }
 }
